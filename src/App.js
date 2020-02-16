ae80e8feda9fbdda4de676d63166ad7474c5a6bf
natashamartinas <59834851+natashamartinas@users.noreply.github.com>
2020-02-16 16:15:32 -0500

    Project finished. Minor tweaks may be needed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Play from './playsign.png';
 import './App.css';
-import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RBCarousel from "react-bootstrap-carousel";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import Vimeo from '@u-wave/react-vimeo';
 
 import { Container, Row, Col} from 'reactstrap';
 import { Jumbotron } from 'reactstrap';
-import { Button } from 'react-bootstrap'; 
+import { Button } from 'react-bootstrap';
 
 const styles = { height: 400, width: "100%" };
 const icon_glass = <span className="fa fa-glass" />;
@@ -54,6 +54,104 @@ var blackHeadingText = {
 var blackParagraph = {
   color: "black", 
   fontSize: 17.5
+}
+
+var huntBackground ={
+  backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_SY1000_SX675_AL_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var huntButton ={
+  backgroundColor:"#0099ff", 
+  borderColor:"#007acc", 
+  margin:5
+}
+
+var nichtsBackground ={
+  backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_SY1000_CR0,0,704,1000_AL_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var nichtsButton ={
+  backgroundColor:"#00b8e6", 
+  borderColor:"#008fb3", 
+  margin:5
+}
+
+var vvBackground ={
+  backgroundImage:"url('https://images-na.ssl-images-amazon.com/images/I/71eoNKrLrQL._RI_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var vvButton ={
+  backgroundColor:"#a3a329", 
+  borderColor:"#808000", 
+  margin:5
+}
+
+var fourthBackground ={
+  backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BOGVhNjUwYTItYmI2NS00ZGI1LWE5ZDQtMWVjMTIyODAzMDI4XkEyXkFqcGdeQXVyMjQ3NzUxOTM@._V1_SY1000_CR0,0,706,1000_AL_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var fourthButton ={
+  backgroundColor:"#336699", 
+  borderColor:"#42668a", 
+  margin:5
+}
+
+var fullmoonBackground ={
+  backgroundImage:"url('https://images-na.ssl-images-amazon.com/images/I/71RW1RGNCpL._RI_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var fullmoonButton ={
+  backgroundColor:"#808080", 
+  borderColor:"#696969", 
+  margin:5
+}
+
+var lwBackground ={
+  backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BY2QzYTQyYzItMzAwYi00YjZlLThjNTUtNzMyMDdkYzJiNWM4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,674,1000_AL_.jpg')",
+  backgroundSize:"cover", 
+  backgroundPosition:"center", 
+  backgroundRepeat:"no-repeat", 
+  overflow:"hidden", 
+  filter: "blur(1rem)", 
+  transform: "scale(1.1)"
+}
+
+var lwButton ={
+  backgroundColor:"#9966ff", 
+  borderColor:"#7733ff", 
+  marginLeft:0,
+  marginTop:10,
+  marginRight:10
 }
 
 
@@ -104,6 +202,32 @@ function SectionRight (props) {
   );
 }
 
+function SlideItem (props) {
+  return (
+    <div className="slideHeight">
+      <div className="slideHeight" style={props.movieBackground}>
+        <div className={props.overlay}></div>
+      </div>
+      <div className="carousel-caption">
+        <Row>
+          <Col xs="6" sm="5" lg="3" style={{padding:10}}>
+            <img className="img-fluid" src={props.moviePosterUrl} />
+          </Col>
+          <Col xs="6" sm="7" lg="9" className="rightie" style={{padding:30}}>
+            <h3 className="movieTitle" style={{color:"white"}}>{props.movieTitle}</h3>
+            <p className="movieDesc" style={{color:"white", opacity:0.7}}>{props.movieDescription}</p>
+            <Button size="sm" style={props.movieBuyButton} variant="primary"><span className="buttonDesc" style={{padding: 0, fontWeight:"bold"}}>
+              <img className="playButtonSizing" style={{marginRight:5}} src={Play} />
+              Buy Now</span>
+            </Button>
+            <Button size="sm" href={props.link} style={{margin:5}} variant="outline-light"><span className="buttonDesc" style={{fontWeight:"bold"}}>Watch Trailer</span></Button>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -113,9 +237,6 @@ export default class App extends React.Component {
   }
   onSelect = (active, direction) => {
     console.log(`active=${active} && direction=${direction}`);
-  };
-  visiableOnSelect = active => {
-    console.log(`visiable onSelect active=${active}`);
   };
   slideNext = () => {
     this.slider.slideNext();
@@ -128,12 +249,6 @@ export default class App extends React.Component {
   };
   autoplay = () => {
     this.setState({ autoplay: !this.state.autoplay });
-  };
-  _changeIcon = () => {
-    let { leftIcon, rightIcon } = this.state;
-    leftIcon = leftIcon ? undefined : icon_glass;
-    rightIcon = rightIcon ? undefined : icon_music;
-    this.setState({ leftIcon, rightIcon });
   };
   render() {
     return (
@@ -157,70 +272,75 @@ export default class App extends React.Component {
                   version={4}
                   indicators={false}
                 >
-                  <div style={{ height: 400}}>
-                    <div className="blur-me" style={{height: 400, backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_SY1000_SX675_AL_.jpg')", backgroundSize:"cover", backgroundPosition:"center", backgroundRepeat:"no-repeat", overflow:"hidden"}}>
-                      
+                  <SlideItem 
+                    movieBackground={huntBackground} 
+                    overlay="huntOverlay"
+                    moviePosterUrl="https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_SY1000_SX675_AL_.jpg" 
+                    movieTitle="Hunt for the Wilderpeople" 
+                    movieDescription="Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows."
+                    movieBuyButton={huntButton}
+                    link="https://vimeo.com/170252585">
+                  </SlideItem>
+                  <SlideItem 
+                    movieBackground={nichtsBackground} 
+                    overlay="nichtsOverlay"
+                    moviePosterUrl="https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_SY1000_CR0,0,704,1000_AL_.jpg" 
+                    movieTitle="Nichts passiert / A Decent Man" 
+                    movieDescription="A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandaries."
+                    movieBuyButton={nichtsButton}
+                    link="https://vimeo.com/155591004">
+                  </SlideItem>
+                  <SlideItem 
+                    movieBackground={vvBackground} 
+                    overlay="vvOverlay"
+                    moviePosterUrl="https://images-na.ssl-images-amazon.com/images/I/71eoNKrLrQL._RI_.jpg" 
+                    movieTitle="Vice Versa" 
+                    movieDescription="Come along with the Good Company crew as they travel throughout the US, Japan, BC and Quebec to showcase skiing in the best way possible."
+                    movieBuyButton={vvButton}
+                    link="https://vimeo.com/180836160">
+                  </SlideItem>
+                  <SlideItem 
+                    movieBackground={fourthBackground} 
+                    overlay="fourthOverlay"
+                    moviePosterUrl="https://m.media-amazon.com/images/M/MV5BOGVhNjUwYTItYmI2NS00ZGI1LWE5ZDQtMWVjMTIyODAzMDI4XkEyXkFqcGdeQXVyMjQ3NzUxOTM@._V1_SY1000_CR0,0,706,1000_AL_.jpg" 
+                    movieTitle="The Fourth Phase" 
+                    movieDescription="From the creators of The Art of FLIGHT, Red Bull Media House presents THE FOURTH PHASE, a snowboarding epic starring iconic athlete Travis Rice."
+                    movieBuyButton={fourthButton}
+                    link="https://vimeo.com/182511910">
+                  </SlideItem>
+                  <SlideItem 
+                    movieBackground={fullmoonBackground} 
+                    overlay="fullmoonOverlay"
+                    moviePosterUrl="https://images-na.ssl-images-amazon.com/images/I/71RW1RGNCpL._RI_.jpg" 
+                    movieTitle="Full Moon" 
+                    movieDescription="Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport."
+                    movieBuyButton={fullmoonButton}
+                    link="https://vimeo.com/180790969">
+                  </SlideItem>
+                  <div className="slideHeight">
+                    <div className="slideHeight" style={lwBackground}>
+                      <div className="lwOverlay"></div>
                     </div>
-                    <div className="carousel-caption">
-                      <Row>
-                        <Col xs="6" sm="5" md="4" lg="3" style={{padding:10, border:"thick solid #0000FF"}}>
-                          <img className="img-fluid" src="https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_SY1000_SX675_AL_.jpg" />
-                        </Col>
-                        <Col xs="6" sm="7" md="8" lg="9" className="leftie" style={{padding:30, border:"thick solid #0000FF"}}>
-                          <h3 style={{color:"white"}}>Hunt for the Wilderpeople</h3>
-                          <p style={{color:"dimgray", opacity:0.7, fontSize:17.5}}>Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows.</p>
-                          <Button style={{backgroundColor:"#4682B4", borderColor:"#5F9EA0", margin:5}} variant="primary">Buy Now</Button>
-                          <Button style={{margin:5}} variant="outline-light"><span style={{fontWeight:"bold"}}>Watch Trailer</span></Button>
-                        </Col>
-                      </Row>
+                      <div className="carousel-center">
+                        <Row>
+                          <Col xs="8" sm="7" lg="9" style={{padding:10}}>
+                            <Vimeo
+                              className="vid-fluid"
+                              height="300%"
+                              video="https://vimeo.com/373140876"
+                              controls="true"
+                              autoplay="false"
+                            />
+                          </Col>
+                          <Col xs="4" sm="5" lg="3" className="rightie" style={{padding:20}}>
+                            <h4 className="movieTitle" style={{color:"white"}}>Little Women <span className="italic">Trailer</span></h4>
+                            <Button size="sm" style={lwButton} variant="primary"><span className="buttonDesc" style={{fontWeight:"bold"}}>
+                              <img className="playButtonSizing" style={{marginRight:5}} src={Play} />
+                              Buy Now</span>
+                            </Button>
+                          </Col>
+                        </Row>
                     </div>
-                  </div>
-                  <div style={{ height: 400}}>
-                    <div className="carousel-caption">FUCK YES</div>
-                    <div className="blur-me" style={{height: 400, backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_SY1000_SX675_AL_.jpg')", backgroundSize:"cover", backgroundPosition:"center", backgroundRepeat:"no-repeat", overflow:"hidden"}}></div>
-                  </div>
-                  <div className="blur-me" style={{ height: 400, backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_SY1000_CR0,0,704,1000_AL_.jpg')", backgroundSize:"cover", backgroundPosition:"center", backgroundRepeat:"no-repeat", overflow:"hidden"}}>
-                    <div className="carousel-caption">Image</div>
-                  </div>
-                  <div className="blur-me" style={{ height: 400, backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_SY1000_CR0,0,704,1000_AL_.jpg')", backgroundSize:"cover", backgroundPosition:"center", backgroundRepeat:"no-repeat", overflow:"hidden"}}>
-                    <div className="carousel-caption">Image</div>
-                  </div>
-                  <div className="blur-me" style={{ height: 400, backgroundImage:"url('https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_SY1000_CR0,0,704,1000_AL_.jpg')", backgroundSize:"cover", backgroundPosition:"center", backgroundRepeat:"no-repeat", overflow:"hidden"}}>
-                    <div className="carousel-caption">Image</div>
-                  </div>
-                  <div style={{ ...styles, backgroundColor: "aqua" }}>
-                    <video
-                      className="carousel-center"
-                      controls
-                      style={{ width: "75%" }}
-                      height="250"
-                    >
-                      <source
-                        src="https://www.w3schools.com/html/mov_bbb.mp4"
-                        type="video/mp4"
-                      />
-                    </video>
-                    <div className="carousel-caption">Video</div>
-                  </div>
-                  <div style={{ ...styles, backgroundColor: "lightpink" }}>
-                    <div className="carousel-center">center Text</div>
-                    <div className="carousel-caption">Text</div>
-                  </div>
-                  <div style={{ ...styles, backgroundColor: "lightblue" }}>
-                    <span>text</span>
-                    <div className="carousel-caption">Text</div>
-                  </div>
-                  <div style={{ ...styles, backgroundColor: "lightblue" }}>
-                    <div className="carousel-center">
-                      <iframe
-                        style={{ width: 500 }}
-                        height="250"
-                        src="https://www.youtube.com/embed/MhkGQAoc7bc?showinfo=0"
-                        frameBorder="0"
-                        allowFullScreen
-                      />
-                    </div>
-                    <div className="carousel-caption">Youtube</div>
                   </div>
                 </RBCarousel>
               </Col>
